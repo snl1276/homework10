05346a42574ccdf9b713ada0e91a9e4aab752a33
snl1276 <98837245+snl1276@users.noreply.github.com>
2022-03-08 20:35:40 +0300

    changed zadanie3.py

--- zadanie3.py
+++ zadanie3.py
@@ -1,4 +1,5 @@
 import json
+import yaml
 data = { 
     'age': 45, 
     'name': 'Peter', 
@@ -13,6 +14,13 @@ data = {
 }
 with open('file.txt', 'w') as fd:
     json.dump(data, fd, indent=4)
+
 with open('file.txt', 'r') as read_file:
-    data = json.load(read_file)
-    print (data)
+    des_data = json.load(read_file)
+    print (des_data)
+print()
+print('Сериализация и десериализация с помощью yaml')
+serial = yaml.dump(data, indent=4)
+print(serial)
+desserial = yaml.load(serial, Loader=yaml.FullLoader)
+print(desserial)
