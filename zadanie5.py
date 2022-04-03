064e2d82005ff38aa4a4374aa407d30616a529e1
snl1276 <98837245+snl1276@users.noreply.github.com>
2022-04-03 17:44:51 +0300

    changed zadanie5.py

--- zadanie5.py
+++ zadanie5.py
@@ -1,12 +1,12 @@
 from openpyxl import load_workbook
 import csv
- 
+
+
 wb_val = load_workbook(filename='tabl.xlsx', data_only=True)
- 
 pointSheets = wb_val.sheetnames
- 
 sheet_val = wb_val[pointSheets[0]]
- 
+
+
 with open('output.csv', 'w', newline='') as csvfile:
     writer = csv.writer(
         csvfile, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -14,7 +14,6 @@ with open('output.csv', 'w', newline='') as csvfile:
         res = []
         for el in element:
             try:
- 
                 res.append(el.value)
             except ValueError:
                 res.append(el.value)
